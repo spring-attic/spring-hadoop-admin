a59302154e2702512b7334dfd090c71c0170830c
Jarred Li <jiali@vmware.com>
2012-05-25 10:11:35 +0800

    typo

--- CLI/src/main/java/org/springframework/data/hadoop/admin/cli/plugin/SpringHadoopAdminBannerProvider.java
+++ CLI/src/main/java/org/springframework/data/hadoop/admin/cli/plugin/SpringHadoopAdminBannerProvider.java
@@ -60,9 +60,9 @@ public class SpringHadoopAdminBannerProvider extends DefaultBannerProvider
 	}
 
 	/* (non-Javadoc)
-	 * @see org.springframework.shell.plugin.BannerProvider#getWelcomMessage()
+	 * @see org.springframework.shell.plugin.BannerProvider#getWelcomeMessage()
 	 */
-	public String getWelcomMessage() {
+	public String getWelcomeMessage() {
 		return "Welcome to Spring Hadoop Admin CLI";
 	}
 	
